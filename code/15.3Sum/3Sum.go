3b8af252684a18ad4e5a2b919b3cefde6837988b
onthroad <164378335@qq.com>
2022-11-30 17:15:09 +0800

    三数之和

---
+++ code/15.3Sum/3Sum.go
@@ -0,0 +1,36 @@
+package code
+
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	result, start, end, index, addNum, length := make([][]int, 0), 0, 0, 0, 0, len(nums)
+	for index = 1; index < length-1; index++ {
+		start, end = 0, length-1
+		if index > 1 && nums[index] == nums[index-1] {
+			start = index - 1
+		}
+		for start < index && end > index {
+			if start > 0 && nums[start] == nums[start-1] {
+				start++
+				continue
+			}
+			if end < length-1 && nums[end] == nums[end+1] {
+				end--
+				continue
+			}
+			addNum = nums[start] + nums[end] + nums[index]
+			if addNum == 0 {
+				result = append(result, []int{nums[start], nums[index], nums[end]})
+				start++
+				end--
+			} else if addNum > 0 {
+				end--
+			} else {
+				start++
+			}
+		}
+	}
+	return result
+
+}
---
+++ code/15.3Sum/3Sum_test.go
@@ -0,0 +1,11 @@
+package code
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestName(t *testing.T) {
+	sum := threeSum([]int{0, 0, 0})
+	fmt.Printf("%v", sum)
+}
